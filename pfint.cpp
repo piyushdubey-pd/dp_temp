061342ef28299346682cae2154875e4a57f58a43
piyushdubey-pd <piyushdubey72090@gmail.com>
2021-06-03 23:58:51 +0530

    3/6/21

---
+++ pfint.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include<cmath>
+#include<vector>
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define ll long long
+#define ld long double
+#define ft first
+#define sd second
+#define pie acos(-1)
+#define in(b,a) ( (b).find(a) != (b).end()) 
+#define fill(a,v) memset(a, v, a.size())
+#define INDEX(arr,ind) (lower_bound(arr.begin(),arr.end(),ind)-arr.begin())
+
+using namespace std;
+ll lcm(ll a,ll b){ return (a*b)/__gcd(a,b); }
+
+
+ll checkprime(ll a){ ll i,b=0; for(i=2;i*i<=a;i++){ if(a%i==0) return 0; } return 1; }
+ll sum_digit(ll n){ ll a=0,i; for(i=n;i>0;i=i/10){ a+=(i%10);} return a; }
+ll num_digit(ll n){ ll a=0,i; for(i=n;i>0;i=i/10){ a++;} return a; }
+ll first_digit(ll n){ ll a=0,i; for(i=n;i>0;i=i/10){ a=i%10;} return a; }
+
+ll get(ll a[],ll i){ ll x=0,j; for(j=i;j>0;j-=j&-j) x+=a[j]; return x; }
+ll flip(ll k){ if(k==0) return 1; return 0; }
+bool checksum(vector<ll> a,ll sum){ ll i,j,n=a.size(); bool b[n+1][sum+1];  for(i=0;i<=n;i++) b[i][0]=true;  for(i=1;i<=sum;i++) b[0][i]=false;  for(i=1;i<=n;i++){  for(j=1;j<=sum;j++){  if(j<a[i-1]) b[i][j]=b[i-1][j];  else b[i][j]=b[i-1][j] || b[i-1][j-a[i-1]]; } }  return b[n][sum]; } 
+ 
+void solve(){
+    string s1,s2;
+    cin>>s1>>s2;
+    int m=s1.size(), n=s2.size();
+    int dp[m+1][n+1];
+    for (int i=0;i<=m;i++)
+    {
+        for(int j=0;j<=n;j++)
+        {
+            if(i==0 || j==0)
+            dp[i][j]=0;
+            else if(s1[i-1]==s2[j-1])
+            dp[i][j]=1+dp[i-1][j-1];
+            else
+            dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    string s="";
+    while(n!=0 || m!=0){
+        if(s1[m-1]==s2[n-1])
+        {
+            s=s1[m-1]+s;
+            n--;m--;
+        }
+        else if(dp[m][n]==dp[m-1][n])
+        {
+            // s+=s1[m-1];
+            m--;
+        }
+        else
+        {
+            // s+=s2[n-1];
+            n--;
+        }
+    }
+    cout<<s<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);
+    int t=1;
+    //cin>>t;
+    while(t--){
+    solve();
+    }	
+}
